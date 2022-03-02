4207a570509032327246b79b93ea2bd0209f9038
Luis Elías Carro <luis.carro@gmail.com>
2022-03-02 17:39:08 -0300

    Hasta video 60

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,39 @@
 // importo referencias
 import React from "react";
+import PropTypes from 'prop-types'
 
 // componente funcional
 const Button = ({ type, text, clickHandler }) => {
+    /* sin llaves*/
     return (
+        <button className={type} onClick={() => clickHandler(text)}>
+
+    {/* con llaves
         <button className={type} onClick={() => {
-            console.log("click en 1")
+            console.log("click button 1")
             clickHandler(text)
         }}>
+    */}
             <span>{ text }</span>
         </button>
     )
 }
 
+// otra forma de hacer lo anterior es sin return y sin llaves
+// cuando el return es la primera línea, es decir que no hay
+// otro algoritmo incluido
+/*
+const Button = ({ type, text, clickHandler }) => (
+    <button className={type} onClick={() => clickHandler(text)}>
+        <span>{ text }</span>
+    </button>
+)
+*/
+
+Button.propTypes = {
+    type: PropTypes.string,
+    text: PropTypes.string.isRequired,
+    clickHandler: PropTypes.func.isRequired
+}
 // exportación
 export default Button
